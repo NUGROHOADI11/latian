f2e541b4f52bdd3757c2e17ef60df04bec6cbb72
NUGROHOADI11 <deeforce112002@gmail.com>
2023-10-21 14:22:44 +0700

    .

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,20 +2,21 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:latian/app/modules/home/models/user.dart';
 import '../controllers/home_controller.dart';
 import 'package:http/http.dart';
 
 class HomeView extends GetView<HomeController> {
   HomeView({Key? key}) : super(key: key);
 
-  List<Map<String, dynamic>> allUser = [];
+  List<UserModel> allUser = [];
 
   Future getAllUser() async {
     try {
       var response = await get(Uri.parse("https://reqres.in/api/users?page=1"));
       List data = (json.decode(response.body) as Map<String, dynamic>)["data"];
       data.forEach((element) {
-        allUser.add(element);
+        allUser.add(UserModel.fromJson(element));
       });
     } catch (e) {
       print("terjadi kesalahan");
@@ -38,16 +39,21 @@ class HomeView extends GetView<HomeController> {
                   child: Text("Loading ...."),
                 );
               } else {
+                if (allUser.length == 0) {
+                  return Center(
+                    child: Text("Tidak ada data"),
+                  );
+                }
                 return ListView.builder(
-                  itemCount: 5,
+                  itemCount: allUser.length,
                   itemBuilder: (context, index) => ListTile(
                     leading: CircleAvatar(
-                      backgroundImage: NetworkImage(allUser[index]['avatar']),
                       backgroundColor: Colors.grey,
+                      backgroundImage: NetworkImage(allUser[index].avatar),
                     ),
                     title: Text(
-                        "${allUser[index]['first_name']} ${allUser[index]['last_name']}"),
-                    subtitle: Text("${allUser[index]['email']}"),
+                        "${allUser[index].firstName} ${allUser[index].lastName}"),
+                    subtitle: Text("${allUser[index].email}"),
                   ),
                 );
               }
